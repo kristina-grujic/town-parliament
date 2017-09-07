5c20cf824801b088cc626f3c6c17d0c466d4ee90
Kristina G <pinksugarpill@gmail.com>
2017-09-07 10:14:53 +0200

    Voting html and scripts

---
+++ Skupstina/static/scripts/vote/vote-ctrl.js
@@ -0,0 +1,60 @@
+'use strict';
+
+angular.module('xmlClientApp')
+	.controller('VoteCrtl', ['$scope', '$uibModal', '$log', '_', '$routeParams', '$location', 'VoteResource',
+	   function($scope, $uibModal, $log, _, $routeParams, $location, VoteResource) {
+
+		var id = $routeParams.id;
+		var tip = $routeParams.tip;
+		var res = id.split(".");
+		var name = $routeParams.name;
+
+		$scope.name = name;
+		$scope.id = id;
+
+		var user = [];
+		var vote = [];
+
+		VoteResource.getUser().then(function(items) {
+			user = items.length;
+			$scope.user = user;
+			VoteResource.getVote(res[0], tip).then(function(tems) {
+
+				vote = tems.length;
+				$scope.vote = vote;
+				$scope.tip = tip;
+				$scope.procenat = (vote/user)*100;
+				$scope.izglasavanje = 100-$scope.procenat;
+
+				if((user/2) >= vote) {
+					$scope.glasanje = false;
+				} else {
+					$scope.glasanje = true;
+				}
+			})
+		})
+
+		$scope.deleteAct = function() {
+			VoteResource.deteAct(res[0]);
+			$location.path('/acts/proposed');
+		}
+
+		$scope.acceptAct = function() {
+			VoteResource.acceptAct(res[0]);
+			$scope.message = true;
+		}
+
+		$scope.acceptAmandman = function() {
+			VoteResource.acceptAmandman(res[0]).then(function(odg) {
+				$scope.messages = true;
+			})
+
+		}
+
+		$scope.deleteAmandman = function() {
+			VoteResource.deleteAmandman(res[0]);
+			$scope.messages = true;
+		}
+
+
+	}])
---
+++ Skupstina/static/scripts/vote/vote-resource.js
@@ -0,0 +1,58 @@
+angular.module('xmlClientApp')
+	.factory('VoteResource', ['Restangular', '_', function(Restangular, _) {
+		'use stict';
+
+		var retVal = {};
+
+		var user = [];
+		var vote = [];
+		var message = [];
+
+		retVal.getUser = function() {
+			var link = 'user/';
+			return Restangular.all(link).getList().then(function(entries) {
+				user = entries;
+				return user;
+			});
+		}
+
+		retVal.getVote = function(id, tip) {
+			var link = link = 'act/adopt/proposal/'+id;
+
+
+			return Restangular.all(link).getList().then(function(entries) {
+				vote = entries;
+				return vote;
+			});
+
+		}
+
+		retVal.acceptAct = function(id) {
+			var link = 'act/accept/'+id;
+			return Restangular.one(link).get();
+
+		}
+
+		retVal.deteAct = function(id) {
+			var link = 'act/delete/'+id;
+			return Restangular.all(link).remove();
+		}
+
+		retVal.acceptAmandman = function(id) {
+			var link = 'amandman/accept/'+id;
+			return Restangular.one(link).get().then(function(resoult) {
+				message = resoult;
+				return message;
+			})
+
+		}
+
+		retVal.deleteAmandman = function(id) {
+			var link = 'amandman/delete/'+id;
+			return Restangular.all(link).remove();
+		}
+
+		return retVal;
+
+
+	}]);
